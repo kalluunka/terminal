960795e7e2189568d3ad1fe916dfcc27138bc257
kalluunka <82115093+kalluunka@users.noreply.github.com>
2024-05-13 19:54:46 +0200

    Update terminalJson.js
    X12 to 12X

--- terminalJson.js
+++ terminalJson.js
@@ -67,7 +67,7 @@ var terminal = {
 		{
 		id: 6,
 		keyword: "LogA1",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "X",
 		type: "[TXT]",
 		text:"<br><br>Okay, nice, its starting! After all those years I finally got it working.<br> So, for everyone who is using this the first time, here is a short explanation. The terminal is a huge Databank with all the important Info about the Humans and stuff. You can use the input field to search for a keyword that might lead you to an article. But be aware, I protected some of them with a password, its important that those informations will not fall in the wrong hands.<br>But it is very important that all this falls in the right hands! Something like that cannot happen again! But I can't say more. I'll continue writing in Log A2.",
@@ -77,7 +77,7 @@ var terminal = {
 		{
 		id: 7,
 		keyword: "LogA2",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "X",
 		type: "[TXT]",
 		text:"<br><br>Welcome back.<br>So, as I said in my last Log, the Information kept in the Terminal should only be viewed by the most inteligent beings. Thats why a lot of the pages are locked with a password. If you can put the puzzle together you might be able to prevent what happened a long time ago from happening to you and your species. Well, I think I will include a little riddle in my next Log, just to see if it works.",
@@ -87,7 +87,7 @@ var terminal = {
 		{
 		id: 7,
 		keyword: "LogA3",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "&#x2605;",
 		type: "[KNL]",
 		text:"<br><br>Like I said, this log is locked with a password. Just type it in the input line if you think you got it. The question is: What does the difficulty type KNL mean?",
@@ -97,7 +97,7 @@ var terminal = {
 		{
 		id: 8,
 		keyword: "Knowledge",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "X",
 		type: "[TXT]",
 		text:"<br><br>Congratulations. But please do not think you are smart just because you could answer my easiest question. Well, at least it seems to work.",
@@ -107,7 +107,7 @@ var terminal = {
 		{
 		id: 9,
 		keyword: "LogA4",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "X",
 		type: "[TXT]",
 		text:"<br><br>Back to it. I began backing up some important Websites that should not get lost. You should be able to access them by typing /websiteName behind the url. You can try it out with /coffeshop, it was the favourite of my creator. I never understood why, the only thing I ever could find out about it was the website and I could not log myself into the member page. Im pretty sure there is more behind all that but I have no Idea what.",
@@ -117,7 +117,7 @@ var terminal = {
 		{
 		id: 10,
 		keyword: "LogA5",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "&#x2605;",
 		type: "[OWP]",
 		text:"<br><br>What is the starting year of the company running the coffe shop website?<br>",
@@ -127,7 +127,7 @@ var terminal = {
 		{
 		id: 11,
 		keyword: "1997",
-		author: "X-12-heat",
+		author: "12-X-heat",
 		level: "X",
 		type: "[TXT]",
 		text:"<br><br>Pinkpaw. What is Pinkpaw? Im 100% positive that is not just a coffeshop. But I yet have to find out what is behind it all. All I know is that Alex, my creator knows the CEO of Pinkpaw, but I have no Idea who he is. Maybe I need to further explore the Terminal? ",
